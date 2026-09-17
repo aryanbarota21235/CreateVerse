304fa6862bac2146ebc71d71f3c5dd5c8a66893c
aryanbarota21235 <aryanbarota21235@gmail.com>
2026-09-17 11:11:22 +0530

    Add Google tag (gtag.js) with ID G-B0X3P49RNE to document head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,6 +116,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationJsonLd) }}
         />
+        {/* Google tag (gtag.js) */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-B0X3P49RNE"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-B0X3P49RNE');`,
+          }}
+        />
         {/* Meta Pixel Code */}
         <Script
           id="meta-pixel"
